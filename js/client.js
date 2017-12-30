281c068f777a63d3575676628131012d46af43b0
mayankmetha <mayankmethad@gmail.com>
2017-12-31 00:45:41 +0530

    firebaseAuth router added

--- js/client.js
+++ js/client.js
@@ -6,11 +6,20 @@ var opn = require('opn');
 var path = require('path');
 var CryptoJS = require('crypto-js');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var https = require('https');
+var forceSsl = require('express-force-ssl');
 var firebaseConfig = require('./config');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+var key = fs.readFileSync(path.join(__dirname+'/../ssl/private.key'));
+var cert = fs.readFileSync(path.join(__dirname+'/../ssl/private.crt'));
+var options = {
+    key: key,
+    cert: cert
+};
 var isAnonymous;
 var uidAnonymous;
 var user;
@@ -18,13 +27,6 @@ var user;
 firebase.initializeApp(firebaseConfig.config);
 
 var db = firebase.database();
-
-function anonymouslySignIn() {
-    firebase.auth().signInAnonymously().catch(function (error) {
-        var error = error.code;
-        var errorMessage = error.message;
-    });
-}
 
 function auth() {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -40,6 +42,22 @@ function auth() {
 }
 
 function server() {
+    https.createServer(options,app).listen(443);
+    app.use(forceSsl);
+    //firebaseAuthCleanUp
+    app.get('/firebaseAuthOut', function (req, res) {
+        firebase.auth().currentUser.delete().then(function() {
+            res.send("Now you can close the browser!");
+        });
+    });
+    //firebaseAuthLogin
+    app.get('/firebaseAuthIn', function (req, res) {
+        firebase.auth().signInAnonymously().catch(function (error) {
+            var error = error.code;
+            var errorMessage = error.message;
+        });
+        res.redirect('/login');
+    });
     //login
     app.get('/login', function (req, res) {
         res.sendFile(path.join(__dirname + '/../main-web/html/login.html'));
@@ -67,10 +85,9 @@ function server() {
             });
         }
     });
-    app.listen(5000, function () {
+    app.listen(80, function () {
         console.log('Opening VoterChain...\nEnter Ctrl+C to Exit...');
-        opn('http://localhost:5000/login');
-        anonymouslySignIn();
+        opn('http://localhost/firebaseAuthIn');
     });
 }
 
