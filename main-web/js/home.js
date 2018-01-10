927c578a39ba8af6d6f8bdad0e2cf8c32811e9e7
mayankmetha <mayankmethad@gmail.com>
2018-01-10 23:18:21 +0530

    Fix pagination

--- main-web/js/home.js
+++ main-web/js/home.js
@@ -1,8 +1,50 @@
-var width = $(window).width();
-var height = $(window).height();
-if (width<=640) {
-    //mobile screen
-} else {
-    //desktop screen
+$(document).ready(function () {
+    var width = $(window).width();
+    var height = $(window).height();
+    if (width <= 640) {
+        //mobile screen
+        mobile();
+    } else {
+        //desktop screen
+        desktop();
+    }
+});
+$(window).resize(function () {
+    var width = $(window).width();
+    var height = $(window).height();
+    var position = $.scrollify.currentIndex();
+    console.log(position);
+    if (width <= 640) {
+        //mobile screen
+        mobile();
+    } else {
+        //desktop screen
+        desktop();
+    }
+});
 
+function mobile() {
+    $.scrollify({
+        section: ".mobile",
+        scrollSpeed: 1100,
+        offset: 0,
+        scrollbars: true,
+        touchScroll: true,
+        updateHash: true,
+        setHeights: false,
+        easing: "easeOutExpo"
+    });
+}
+
+function desktop() {
+    $.scrollify({
+        section: ".desktop",
+        scrollSpeed: 1100,
+        offset: 0,
+        scrollbars: true,
+        touchScroll: true,
+        updateHash: true,
+        setHeights: false,
+        easing: 'easeOutExpo'
+    });
 }
