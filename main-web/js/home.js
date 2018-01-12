558025a12351205a20dea67ab690659ad0a67fba
mayankmetha <mayankmethad@gmail.com>
2018-01-12 21:54:51 +0530

    go to start page on resize

--- main-web/js/home.js
+++ main-web/js/home.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
 $(window).resize(function () {
     var width = $(window).width();
     var height = $(window).height();
+    $.scrollify.instantMove("#1");
     if (width <= 640) {
         //mobile screen
         mobile();
