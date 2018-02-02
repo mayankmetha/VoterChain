f458f4fcb570ff06c5eea29c8906d6c8f148c88c
mayankmetha <mayankmethad@gmail.com>
2018-02-02 16:31:00 +0530

    revamped main.js

--- main.js
+++ main.js
@@ -5,38 +5,39 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var os = require('os');
 
-console.log('Checking OS Platform...');
-if(os.platform() !== "linux" && os.platform() !== "darwin") {
+var platform = os.platform();
+var browser;
+if(platform !== "linux" && platform !== "darwin") {
     console.log(os.platform()+' not supported...');
     process.exit(0);
 }
-var platform = os.platform();
-console.log(platform+' supported...');
 
 if (process.argv[2] === "admin") {
-    var browser;
-    if(process.argv[3] === "chrome") {
-        if(platform === "linux") {
-            browser = "google-chrome";
-        } else {
-            browser = "google chrome"
-        }
-    } else if(process.argv[3] === "firefox") {
-        browser = "firefox";
-    } else if(process.argv[3] === "safari" && platform === "darwin") {
-        browser = "safari";
-    } else {
-        if(platform === "linux") {
-            console.log('Browser supported are chrome or firefox...');
-        } else {
-            console.log('Browser supported are chrome, safari or firefox...');
-        }
-        process.exit(0);
-    }
+    selectBrowser();
     admin.server(browser);
     admin.cleanUp();
 } else if (process.argv[2] === "client") {
-    var browser;
+    selectBrowser();
+    client.server(browser);
+    client.cleanUp();
+} else if(process.argv[2] === "help") {
+    helpMes();
+} else {
+    console.log("Command Usage error...\n");
+    helpMes();
+}
+
+function helpMes() {
+    if(platform === "linux") {
+        console.log("USAGE:\tsudo npm start [client|admin] [firefox|chrome]");
+    } else if(platform === "darwin") {
+        console.log("USAGE:\tsudo npm start [client|admin] [safari|firefox|chrome]");
+    }
+    console.log("HELP:\tnpm start help");
+    process.exit(0);
+}
+
+function selectBrowser() {
     if(process.argv[3] === "chrome") {
         if(platform === "linux") {
             browser = "google-chrome";
@@ -48,33 +49,7 @@ if (process.argv[2] === "admin") {
     } else if(process.argv[3] === "safari" && platform === "darwin") {
         browser = "safari";
     } else {
-        if(platform === "linux") {
-            console.log('Browser supported are chrome or firefox...');
-        } else {
-            console.log('Browser supported are chrome, safari or firefox...');
-        }
-        process.exit(0);
+        console.log("Command Usage error...\n");
+        helpMes();
     }
-    client.server(browser);
-    client.cleanUp();
-} else {
-    var app = express();
-    app.use(bodyParser.json());
-    app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain.getChain())));
-    app.post('/mineBlock', (req, res) => {
-        var newBlock = blockchain.genBlocks(req.body.uid, req.body.eleid, req.body.conid, req.body.parid);
-        if (blockchain.addBlock(newBlock)) {
-            blockchain.broadcast(blockchain.responseLatestMsg());
-            console.log('block added: ' + JSON.stringify(newBlock));
-        } else {
-            console.log('cannot add illegal block!');
-        }
-        res.send();
-    });
-    app.post('/addPeer', (req, res) => {
-        blockchain.connectToPeer(req.body.peer);
-        res.send();
-    });
-    app.listen(process.argv[2], () => console.log('Listening http on port: ' + process.argv[2]));
-    blockchain.server('127.0.0.1', process.argv[3]);
 }
