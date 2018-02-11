ddea73ccfe0eecc4c30dbd2259f9a828450a8338
mayankmetha <mayankmethad@gmail.com>
2018-02-11 18:52:48 +0530

    fix

--- verify.sh
+++ verify.sh
@@ -1,5 +1,6 @@
 cat filelist.sha256 | cut -d" " -f1 > i
 cat filelist.sha256 | cut -d" " -f3 | xargs sha256sum | cut -d" " -f1 > j
+rm filelist.sha256
 if diff i j > /dev/null; then
     rm i j
     exit 0
