0f7163a108949981dce495670758bce8b2117eea
mayankmetha <mayankmethad@gmail.com>
2018-02-11 17:48:07 +0530

    added file checksums

---
+++ verify.sh
@@ -0,0 +1,10 @@
+cat filelist.sha256 | cut -d" " -f1 > i
+cat filelist.sha256 | cut -d" " -f3 | xargs sha256sum | cut -d" " -f1 > j
+if diff i j > /dev/null; then
+    rm i j
+    exit 0
+else
+    rm i j
+    echo "Error:Files have been modified"
+    exit 1
+fi
