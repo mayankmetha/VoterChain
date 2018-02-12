ec48b1aa36600f5783f9201824d53b41461a2d5a
mayankmetha <mayankmethad@gmail.com>
2018-02-12 08:55:45 +0530

    fix commands for macOS

--- verify.sh
+++ verify.sh
@@ -1,5 +1,5 @@
 cat filelist.sha256 | cut -d" " -f1 > i
-cat filelist.sha256 | cut -d" " -f3 | xargs sha256sum | cut -d" " -f1 > j
+cat filelist.sha256 | cut -d" " -f3 | xargs openssl sha256 | cut -d" " -f2 > j
 rm filelist.sha256
 if diff i j > /dev/null; then
     rm i j
