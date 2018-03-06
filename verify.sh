ec2e8a6b0e1b8ed1fd90734780780f9b364af5dd
mayankmetha <mayankmethad@gmail.com>
2018-03-06 17:00:59 +0530

    fix

--- verify.sh
+++ verify.sh
@@ -3,10 +3,11 @@ cat filelist.sha256 | cut -d" " -f3 | xargs openssl sha256 | cut -d" " -f2 > j
 rm filelist.sha256
 if diff i j > /dev/null; then
     rm i j
-    echo -e "\033[2J\033[u"
+    clear
     exit 0
 else
     rm i j
-    echo -e "\033[2J\033[u\033[1;31mError:Files have been modified\033[0m"
+    clear
+    echo -e "\033[1;31mError:Files have been modified\033[0m"
     exit 1
 fi
