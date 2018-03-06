6c2d083e35811f0abb42fe53c05e28175d9f1846
mayankmetha <mayankmethad@gmail.com>
2018-03-06 16:52:33 +0530

    beautify prestart

--- verify.sh
+++ verify.sh
@@ -3,9 +3,10 @@ cat filelist.sha256 | cut -d" " -f3 | xargs openssl sha256 | cut -d" " -f2 > j
 rm filelist.sha256
 if diff i j > /dev/null; then
     rm i j
+    echo -e "\033[2J\033[u"
     exit 0
 else
     rm i j
-    echo "Error:Files have been modified"
+    echo -e "\033[2J\033[u\033[1;31mError:Files have been modified\033[0m"
     exit 1
 fi
