5d5cba363883f5c97f84c69b27c903ceaad3a588
mayankmetha <mayankmethad@gmail.com>
2018-03-31 16:26:34 +0530

    added Ubuntu installer

---
+++ setup/Ubuntu/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cat ../header
+cd ../..
+echo "[+] Checking Node.js"
+node -v > /dev/null
+if [ $? -ne 0 ]
+then
+    echo "[!] Node.js not found"
+    echo "[!] Installing Node.js"
+    curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+else
+    echo "[+] Node.js found"
+fi
+echo "[+] Installing dependencies"
+npm install > /dev/null
+echo "[+] Dependencies installed"
+mkdir ssl
+echo "[+] Generating SSL files"
+openssl req -new -newkey rsa:2048 -nodes -out ./ssl/mydomain.csr -keyout ./ssl/private.key
+openssl x509 -signkey ./ssl/private.key -in ./ssl/mydomain.csr -req -days 1000 -out ./ssl/private.crt
+echo "[+] SSL files generated"
+echo "[+] Generating cryptofiles"
+openssl enc -aes-256-cfb -P -md sha256 | tail -2 | head -1 | cut -d"=" -f2 > ./ssl/blockchain.key
+echo "[+] Generated cryptofiles"
+echo "[+] Generating Firebase config"
+read -p "Enter apiKey: " apiKey
+read -p "Enter authDomain: " authDomain
+read -p "Enter databaseURL: " databaseURL
+read -p "Enter projectId: " projectId
+read -p "Enter storageBucket: " storageBucket
+read -p "Enter messagingSenderId: " messagingSenderId
+echo "var config = {apiKey: "$apiKey",authDomain: "$authDomain",databaseURL: "$databaseURL",projectId: "$projectId",storageBucket: "$storageBucket",messagingSenderId: "$messagingSenderId"};firebase.initializeApp(config);" > ./admin-web/js/config.js
+echo "var config = {apiKey: "$apiKey",authDomain: "$authDomain",databaseURL: "$databaseURL",projectId: "$projectId",storageBucket: "$storageBucket",messagingSenderId: "$messagingSenderId"};module.exports = {config: config};" > ./js/config.js
+echo "[+] Generated files"
