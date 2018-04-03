232c8e23d81eed4c3205479a94786eca2a64e511
mayankmetha <mayankmethad@gmail.com>
2018-04-03 20:27:16 +0530

    fix firebase config installation

--- setup/Ubuntu/setup.sh
+++ setup/Ubuntu/setup.sh
@@ -30,6 +30,6 @@ read -p "Enter databaseURL: " databaseURL
 read -p "Enter projectId: " projectId
 read -p "Enter storageBucket: " storageBucket
 read -p "Enter messagingSenderId: " messagingSenderId
-echo "var config = {apiKey: "$apiKey",authDomain: "$authDomain",databaseURL: "$databaseURL",projectId: "$projectId",storageBucket: "$storageBucket",messagingSenderId: "$messagingSenderId"};firebase.initializeApp(config);" > ./admin-web/js/config.js
-echo "var config = {apiKey: "$apiKey",authDomain: "$authDomain",databaseURL: "$databaseURL",projectId: "$projectId",storageBucket: "$storageBucket",messagingSenderId: "$messagingSenderId"};module.exports = {config: config};" > ./js/config.js
+echo "var config = {apiKey: \"$apiKey\",authDomain: \"$authDomain\",databaseURL: \"$databaseURL\",projectId: \"$projectId\",storageBucket: \"$storageBucket\",messagingSenderId: \"$messagingSenderId\"};firebase.initializeApp(config);" > ./admin-web/js/config.js
+echo "var config = {apiKey: \"$apiKey\",authDomain: \"$authDomain\",databaseURL: \"$databaseURL\",projectId: \"$projectId\",storageBucket: \"$storageBucket\",messagingSenderId: \"$messagingSenderId\"};module.exports = {config: config};" > ./js/config.js
 echo "[+] Generated files"
