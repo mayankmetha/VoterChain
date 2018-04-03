4e50d1dc4b22b73f35b01a518d0478037816e8d0
mayankmetha <mayankmethad@gmail.com>
2018-04-03 22:02:51 +0530

    fix setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,15 +4,15 @@ cat ./setup/common/header
 if [[ "$OSTYPE" == "darwin"* ]]
 then
 echo "[!] OS Detected: MacOS/OS X"
-bash ./setup/common/homebrew.sh
-bash ./setup/common/macNode.sh
+bash ./setup/mac/homebrew.sh
+bash ./setup/mac/node.sh
 elif [[ "$OSTYPE" == "linux-gnu" ]]
 then
 echo "[!] OS Detected: Linux"
 if [[ "`grep ID_LIKE /etc/*-release | cut -d'=' -f2`" == "debian" ]]
 then
 echo "[!] Distro: Ubuntu/Debian/Linux Mint/ElementryOS"
-bash ./setup/common/debianNode.sh
+bash ./setup/debian/node.sh
 else
 echo "[!] Distro not tested!"
 fi
