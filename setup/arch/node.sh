b84c8b8fd07900187e6ed65fe26f3a784999be0b
mayankmetha <mayankmethad@gmail.com>
2018-04-04 13:38:29 +0530

    fix archlinux node installer

--- setup/arch/node.sh
+++ setup/arch/node.sh
@@ -1,1 +1,10 @@
-sudo pacman -S nodejs npm
+echo "[+] Checking Node.js"
+node -v > /dev/null
+if [ $? -ne 0 ]
+then
+    echo "[!] Node.js not found"
+    echo "[!] Installing Node.js"
+    sudo pacman -S nodejs npm
+else
+    echo "[+] Node.js found"
+fi
