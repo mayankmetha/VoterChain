d72b7d1d9df64910425d426e8c6623ed5634dc24
mayankmetha <mayankmethad@gmail.com>
2018-04-12 17:26:08 +0530

    fix repeated codes

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,18 +5,15 @@ if [[ "$OSTYPE" == "darwin"* ]]
 then
 echo "[+] OS: macOS/OS X"
 bash ./setup/mac/node.sh
-bash ./setup/common/npm.sh
-bash ./setup/common/ssl.sh
-bash ./setup/common/aes.sh
-bash ./setup/common/firebase.sh
 elif [[ "$OSTYPE" == "linux-gnu" || "`grep Ubuntu /etc/issue.net`" == "Ubuntu" ]]
 then
 echo "[+] OS: Ubuntu Linux"
 bash ./setup/ubuntu/node.sh
+else
+echo "[+] OS not supported"
+exit 0
+fi
 bash ./setup/common/npm.sh
 bash ./setup/common/ssl.sh
 bash ./setup/common/aes.sh
 bash ./setup/common/firebase.sh
-else
-echo "[+] OS not supported"
-fi
