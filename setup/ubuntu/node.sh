789ae4c23018086f06339dad0756a715ffdd9796
mayankmetha <mayankmethad@gmail.com>
2018-04-14 20:41:52 +0530

    added options for older distros

--- setup/ubuntu/node.sh
+++ setup/ubuntu/node.sh
@@ -2,6 +2,8 @@ echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Nod
 if ! type node > /dev/null 2>&1
 then
     echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m NodeJS         \033[0m\033[1;37m:\033[0m\033[1;36m Installing\033[0m                       "
+    sudo apt-get update > /dev/null
+    sudo apt-get install software-properties-common > /dev/null
     sudo add-apt-repository -y -r ppa:chris-lea/node.js > /dev/null
     sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list > /dev/null
     sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list.save > /dev/null
