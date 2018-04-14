c6a60e8bed7124bf85257f2e41715615150a2b8d
mayankmetha <mayankmethad@gmail.com>
2018-04-14 12:19:48 +0530

    fix

--- setup/ubuntu/node.sh
+++ setup/ubuntu/node.sh
@@ -1,7 +1,7 @@
 echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m NodeJS\033[0m\033[1;37m:\033[0m\033[1;36m Checking\033[0m"
 if ! type node > /dev/null 2>&1
 then
-    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m NodeJS\033[0m\033[1;37m:\033[0m\033[1;36m Installing\033[0m                       \n"
+    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m NodeJS\033[0m\033[1;37m:\033[0m\033[1;36m Installing\033[0m                       "
     sudo add-apt-repository -y -r ppa:chris-lea/node.js > /dev/null
     sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list > /dev/null
     sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list.save > /dev/null
