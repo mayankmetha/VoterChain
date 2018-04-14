1abb1569ec877ad7d30c89473d6dc22ee1054755
mayankmetha <mayankmethad@gmail.com>
2018-04-14 12:22:55 +0530

    cleanup ui

--- setup/common/npm.sh
+++ setup/common/npm.sh
@@ -1,4 +1,4 @@
 cd $1
-echo -ne "\r[\033[0;31m✘\033[0m] Dependencies: Installing" 
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Dependencies\033[0m\033[1;37m:\033[0m\033[1;36m Installing\033[0m"
 npm install > /dev/null 2>&1
-echo -ne "\r[\033[1;32m✔\033[0m] Dependencies: Installed               \n"
+echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Dependencies\033[0m\033[1;37m:\033[0m\033[1;36m Installed\033[0m                         \n"  
