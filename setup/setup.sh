1f6411cef3bf4cf8706d9b65d8dd6f05054ae72a
mayankmetha <mayankmethad@gmail.com>
2018-04-14 21:01:13 +0530

    reverted back

--- setup/setup.sh
+++ setup/setup.sh
@@ -25,10 +25,6 @@ then
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Ubuntu\033[0m"
         bash $DIR/ubuntu/node.sh
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "elementary" ]]
-    then
-        echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Elementary\033[0m"
-        bash $DIR/ubuntu/node.sh
     fi
 else
     echo -e "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Not Supported\033[0m"
