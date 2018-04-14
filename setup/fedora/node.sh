7a4601926de60e9134978b3510da9ed492c833c2
mayankmetha <mayankmethad@gmail.com>
2018-04-14 21:32:54 +0530

    fix fedora

--- setup/fedora/node.sh
+++ setup/fedora/node.sh
@@ -3,6 +3,7 @@ if ! type node > /dev/null 2>&1
 then
     echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m NodeJS         \033[0m\033[1;37m:\033[0m\033[1;36m Installing\033[0m                       "
     curl --silent --location https://rpm.nodesource.com/setup_9.x | sudo bash - > /dev/null 2>&1
+    sudo yum install gcc-c++ make > /dev/null 2>&1
     sudo yum -y install nodejs > /dev/null 2>&1
     echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m NodeJS         \033[0m\033[1;37m:\033[0m\033[1;36m Installed\033[0m                         \n" 
 else
