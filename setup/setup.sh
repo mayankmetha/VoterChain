2376758928177997168c42d14fad951ce41c180f
mayankmetha <mayankmethad@gmail.com>
2018-04-14 18:35:03 +0530

    made it flexible to add more distros

--- setup/setup.sh
+++ setup/setup.sh
@@ -18,10 +18,14 @@ if [[ "$OSTYPE" == "darwin"* ]]
 then
     echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m macOS/OS X\033[0m"
     bash $DIR/mac/node.sh
-elif [[ "$OSTYPE" == "linux-gnu" || "`grep Ubuntu /etc/issue.net`" == "Ubuntu" ]]
+elif [[ "$OSTYPE" == "linux-gnu" ]]
 then
-    echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Ubuntu Linux\033[0m"
-    bash $DIR/ubuntu/node.sh
+    echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Linux\033[0m"
+    if [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "Ubuntu" ]]
+    then
+        echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Ubuntu\033[0m"
+        bash $DIR/ubuntu/node.sh
+    fi
 else
     echo -e "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Not Supported\033[0m"
     exit 0
