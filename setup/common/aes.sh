ee423d36bea8da16371723849cdb47d52e26604d
mayankmetha <mayankmethad@gmail.com>
2018-04-14 14:43:18 +0530

    cleanUI

--- setup/common/aes.sh
+++ setup/common/aes.sh
@@ -1,8 +1,10 @@
-if [[ ! -f ./ssl/blockchain.key ]]
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m CryptoKey\033[0m\033[1;37m:\033[0m\033[1;36m Checking\033[0m"
+if [[ ! -f "$1/ssl/blockchain.key" ]]
 then
-echo "[+] Generating cryptofiles"
-openssl enc -aes-256-cfb -P -md sha256 | tail -2 | head -1 | cut -d"=" -f2 > ./ssl/blockchain.key
-echo "[+] Generated cryptofiles"
+    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m CryptoKey\033[0m\033[1;37m:\033[0m\033[1;36m Generating\033[0m                       "    
+    pass="`openssl rand -base64 12`"
+    openssl enc -aes-256-cfb -P -k $pass -md sha256 | tail -2 | head -1 | cut -d"=" -f2 > "$1/ssl/blockchain.key"
+    echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m CryptoKey\033[0m\033[1;37m:\033[0m\033[1;36m Generated\033[0m                         \n"      
 else
-echo "[+] Cryptofiles found"
+    echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m CryptoKey\033[0m\033[1;37m:\033[0m\033[1;36m Generated\033[0m                         \n"      
 fi
--- setup/common/ssl.sh
+++ setup/common/ssl.sh
@@ -1,13 +1,15 @@
-if [[ ! -d ssl ]]
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m SSL\033[0m\033[1;37m:\033[0m\033[1;36m Checking\033[0m"
+if [[ ! -d "$1/ssl" ]]
 then
-mkdir ssl
+    mkdir $1/ssl
 fi
-if [[ ! -f ./ssl/mydomain.csr || ! -f ./ssl/private.key || ! -f ./ssl/private.crt ]] 
+if [[ ! -f "$1/ssl/mydomain.csr" || ! -f "$1/ssl/private.key" || ! -f "$1/ssl/private.crt" ]] 
 then
-echo "[+] Generating SSL files"
-openssl req -new -newkey rsa:2048 -nodes -out ./ssl/mydomain.csr -keyout ./ssl/private.key
-openssl x509 -signkey ./ssl/private.key -in ./ssl/mydomain.csr -req -days 1000 -out ./ssl/private.crt
-echo "[+] SSL files generated"
+    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m SSL\033[0m\033[1;37m:\033[0m\033[1;36m Generating\033[0m                       "    
+    openssl req -new -newkey rsa:2048 -nodes -out "$1/ssl/mydomain.csr" -keyout "$1/ssl/private.key" -subj "/CN=VoterChain" > /dev/null 2>&1
+    openssl x509 -signkey "$1/ssl/private.key" -in "$1/ssl/mydomain.csr" -req -days 1000 -out "$1/ssl/private.crt" > /dev/null 2>&1
+    echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m SSL\033[0m\033[1;37m:\033[0m\033[1;36m Generated\033[0m                         \n"  
+
 else
-echo "[+] SSL files found"
+    echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m SSL\033[0m\033[1;37m:\033[0m\033[1;36m Generated\033[0m                         \n"  
 fi
