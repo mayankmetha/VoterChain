be6b6bdbea138b2c1edceef8c0720f0f17706c9f
mayankmetha <mayankmethad@gmail.com>
2018-04-14 20:01:36 +0530

    Added support for LinuxMint and LMDE

--- setup/setup.sh
+++ setup/setup.sh
@@ -25,6 +25,9 @@ then
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Ubuntu\033[0m"
         bash $DIR/ubuntu/node.sh
+    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "LinuxMint" ]]
+        echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Ubuntu\033[0m"
+        bash $DIR/ubuntu/node.sh
     fi
 else
     echo -e "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Not Supported\033[0m"
