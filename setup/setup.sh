0fd90066cdd67e57f5b9989417b83d92a72176d1
mayankmetha <mayankmethad@gmail.com>
2018-04-15 12:56:43 +0530

    fix linux detection

--- setup/setup.sh
+++ setup/setup.sh
@@ -18,7 +18,7 @@ if [[ "$OSTYPE" == "darwin"* ]]
 then
     echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m macOS/OS X\033[0m"
     bash $DIR/mac/node.sh
-elif [[ "$OSTYPE" == "linux-gnu" ]]
+elif [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux" ]]
 then
     echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Linux\033[0m"
     if [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "Ubuntu" ]]
@@ -37,6 +37,9 @@ then
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m OpenSUSE\033[0m"
         bash $DIR/fedora/node.sh
+    else
+        echo -e "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Not Supported\033[0m"
+        exit 0
     fi
 else
     echo -e "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Not Supported\033[0m"
