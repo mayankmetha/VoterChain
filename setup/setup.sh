c7c4139540ee6124a9edda91c9f28d1f0215d3da
mayankmetha <mayankmethad@gmail.com>
2018-04-22 15:25:14 +0530

    fix

--- setup/setup.sh
+++ setup/setup.sh
@@ -22,57 +22,57 @@ elif [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux" ]]
 then
     echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m OS             \033[0m\033[1;37m:\033[0m\033[1;36m Linux\033[0m"
     #ubuntu-ubuntu
-    if [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "Ubuntu" ]]
+    if [[ "`lsb_release -i | cut -f2`" == "Ubuntu" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Ubuntu\033[0m"
         bash $DIR/ubuntu/node.sh
     #mint-mint
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "LinuxMint" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "LinuxMint" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m LinuxMint/LMDE\033[0m"
         bash $DIR/mint/node.sh
     #elementary-mint
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "elementary" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "elementary" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Elementary\033[0m"
         bash $DIR/mint/node.sh
     #boss-mint
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "BOSS" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "BOSS" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m BOSS\033[0m"
         #bash $DIR/mint/node.sh
     #debian-debian
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "Debian" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "Debian" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Debian\033[0m"
         bash $DIR/debian/node.sh
     #solus-solus
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "Solus" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "Solus" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Solus\033[0m"
         bash $DIR/solus/node.sh
     #fedora-fedora
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "Fedora" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "Fedora" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Fedora\033[0m"
         bash $DIR/fedora/node.sh
     #cent-fedora
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "CentOS" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "CentOS" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m CentOS\033[0m"
         bash $DIR/fedora/node.sh
     #opensuse-suse
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "openSUSE project" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "openSUSE project" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m OpenSUSE\033[0m"
         bash $DIR/opensuse/node.sh
     #arch-arch
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "Arch" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "Arch" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Arch\033[0m"
         bash $DIR/arch/node.sh
     #voidLinux-void
-    elif [[ "`lsb_release -i | cut -d":" -f2 | cut --complement -c1`" == "VoidLinux" ]]
+    elif [[ "`lsb_release -i | cut -f2`" == "VoidLinux" ]]
     then
         echo -e "\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Distro         \033[0m\033[1;37m:\033[0m\033[1;36m Void Linux\033[0m"
         bash $DIR/void/node.sh
