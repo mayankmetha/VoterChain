faa3eee9e92500695f006016aac5e2039e898c8c
mayankmetha <mayankmethad@gmail.com>
2018-04-25 18:27:11 +0530

    make start script as setup

--- verify.sh
+++ verify.sh
@@ -4,6 +4,13 @@ rm filelist.sha256
 if diff i j > /dev/null; then
     rm i j
     clear
+    echo -e "\033[1;37m---------------------------------------------------\033[0m"
+    echo -ne "\033[1;33m"
+    cat setup/common/header
+    echo -e "\033[0m"
+    echo -e "\033[1;37m---------------------------------------------------\033[0m"
+    echo -e "\033[1;37m+++++\033[1;31mhttps://github.com/mayankmetha/VoterChain\033[1;37m+++++\033[0m"
+    echo -e "\033[1;37m---------------------------------------------------\033[0m"
     exit 0
 else
     rm i j
